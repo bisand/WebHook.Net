e2c5865e91878a8db1ddf2db608ca52833f356a0
André Biseth <andre@biseth.net>
2018-02-15 22:28:03 +0100

    Small changes

--- Controllers/BlogEventsController.cs
+++ Controllers/BlogEventsController.cs
@@ -40,31 +40,32 @@ namespace WebHook.Net.Controllers
             dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(json);
             string repoName = obj.repository.name;
             string repoUrl = obj.repository.clone_url;
+            string tempDirName = Guid.NewGuid().ToString();
 
             ThreadPool.QueueUserWorkItem(delegate {
-                BuildApplication(repoName, repoUrl);
-                DeployApplication(repoName, repoUrl);
+                BuildApplication(tempDirName, repoName, repoUrl);
+                DeployApplication(tempDirName, repoName, repoUrl);
             });
 
             return Ok(new { Ok = true });
         }
         
-        private static void BuildApplication(string repoName, string repoUrl)
+        private static void BuildApplication(string tempDirName, string repoName, string repoUrl)
         {
-            ("rm -rf /tmp/" + repoName).Bash();
-            ("git clone " + repoUrl + " /tmp/" + repoName).Bash();
-            ("cd /tmp/" + repoName + "/ && npm install").Bash();
-            ("cd /tmp/" + repoName + "/ && dotnet publish -o /tmp/" + repoName + "_publish/").Bash();
-            ("cd /tmp/" + repoName + "/ && tar -zcvf /tmp/" + repoName + "_publish.tar.gz /tmp/" + repoName + "_publish").Bash();
+            ($"rm -rf /tmp/{tempDirName}/{repoName}").Bash();
+            ($"git clone {repoUrl}/tmp/{tempDirName}/{repoName}").Bash();
+            ($"cd /tmp/{tempDirName}/{repoName}/ && npm install").Bash();
+            ($"cd /tmp/{tempDirName}/{repoName}/ && dotnet publish -o /tmp/{repoName}_publish/").Bash();
+            ($"cd /tmp/{tempDirName}/{repoName}/ && tar -zcvf /tmp/{tempDirName}/{repoName}_publish.tar.gz /tmp/{tempDirName}/{repoName}_publish").Bash();
         }
 
-        private static void DeployApplication(string repoName, string repoUrl)
+        private static void DeployApplication(string tempDirName, string repoName, string repoUrl)
         {
             if(!Directory.Exists("./sshkeys"))
-                "mkdir ./sshkeys/".Bash();
+                ($"mkdir ./sshkeys/").Bash();
             
             if(!System.IO.File.Exists("./sshkeys/id_rsa"))
-                "cp ~/.ssh/id_rsa* ./sshkeys/".Bash();
+                ($"cp ~/.ssh/id_rsa* ./sshkeys/").Bash();
 
             using (var sshClient = new SshClient(_sshConfig.Host, _sshConfig.Username, new []{new PrivateKeyFile(_sshConfig.KeyFile)}))
             {
@@ -73,12 +74,12 @@ namespace WebHook.Net.Controllers
                 {
                     scpClient.Connect();
                     Debug.Print(sshClient.RunCommand("ls -hal /tmp/").Execute());
-                    sshClient.RunCommand("rm -rf /tmp/" + repoName).Execute();
-                    scpClient.Upload(new FileInfo("/tmp/" + repoName + "_publish.tar.gz"), "/tmp/" + repoName + "_publish.tar.gz");
-                    sshClient.RunCommand("tar zxf /tmp/" + repoName + "_publish.tar.gz --directory /tmp/" + repoName + "_publish").Execute();
-                    sshClient.RunCommand("cd /tmp/" + repoName + "_publish && dotnet restore").Execute();
-                    sshClient.RunCommand("cd /tmp/" + repoName + "_publish && dotnet build").Execute();
-                    sshClient.RunCommand("cd /tmp/" + repoName + "_publish && dotnet publish").Execute();
+                    sshClient.RunCommand($"rm -rf /tmp/{repoName}").Execute();
+                    scpClient.Upload(new FileInfo($"/tmp/{tempDirName}/{repoName}_publish.tar.gz"), $"/tmp/{repoName}_publish.tar.gz");
+                    sshClient.RunCommand($"tar zxf /tmp/{repoName}_publish.tar.gz --directory /tmp/{repoName}_publish").Execute();
+                    sshClient.RunCommand($"cd /tmp/{repoName}_publish && dotnet restore").Execute();
+                    sshClient.RunCommand($"cd /tmp/{repoName}_publish && dotnet build").Execute();
+                    sshClient.RunCommand($"cd /tmp/{repoName}_publish && dotnet publish").Execute();
                     
                     //TODO Do the dockerfile build process. The files are on Gollum!
                     
